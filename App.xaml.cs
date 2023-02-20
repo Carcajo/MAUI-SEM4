ed530f343e23a683d71d3055f433514fa61ca48f
Maxim Puhov <93794796+Carcajo@users.noreply.github.com>
2023-02-20 21:55:34 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace Calculator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        const int newWidth = 360;
+        const int newHeight = 600;
+
+        window.Width = newWidth;
+        window.Height = newHeight;
+
+        window.MinimumHeight = newHeight;
+        window.MinimumWidth = newWidth;
+
+        return window;
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Calculator.Pages.CurrencyConverter;
+using Calculator.Pages.CurrencyConverter.Services;
+using Calculator.Pages.SQLiteDemo.Services;
+using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+
+namespace Calculator;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+		builder.Services.AddTransient<IDbService, SQLiteService>();
+        builder.Services.AddSingleton<SQLiteDemoPage>();
+        builder.Services.AddTransient<IRateService, RateService>();
+        builder.Services.AddHttpClient<IRateService, RateService>(opt => opt.BaseAddress = new Uri("https://www.nbrb.by/api/exrates/rates"));
+        builder.Services.AddSingleton<CurrencyConverter>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
